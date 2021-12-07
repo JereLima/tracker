071f852d94bbb5b5761f41a7eaedbec31c07c4cf
Jeremias Santos Lima <jeremias.lima@agv.com.br>
2021-12-07 17:34:47 -0300

    detalhes fim

--- src/screens/OrderDetails/index.js
+++ src/screens/OrderDetails/index.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { Linking } from "react-native";
 import {
   Container,
+  Scroll,
   ContainerItem,
-  StatusImage,
-  Row,
   Column,
   Status,
   OrderNumber,
@@ -12,10 +12,13 @@ import {
   ContainerProducts,
   FlatList,
   TitleProducts,
-  ColumnProducts,
   Products,
   ContainerPod,
+  TitlePod,
   ContainerOcurrence,
+  TitleOcurrence,
+  DescriptionOcurrence,
+  Row,
 } from "./styles";
 import { Header, Spacing } from "../../components";
 import { useRoute } from "@react-navigation/native";
@@ -24,6 +27,7 @@ import Ocurrence from "../../images/icons/ocurrence.svg";
 import Truck from "../../images/icons/truck.svg";
 import Location from "../../images/icons/pin.svg";
 import Box from "../../images/icons/box.svg";
+import Pod from "../../images/icons/pod.svg";
 
 import { useTheme } from "styled-components";
 import { shadow } from "../../theme/shadows";
@@ -34,47 +38,79 @@ export default function OrderDetails() {
   const route = useRoute();
   const { item } = route.params;
   const status = item.statusOrder.statusTracking;
+  const ocurrence = item.statusOrder.occurrence;
+  const descripition = item.statusOrder.descriptionOccurrence;
+  const proof = item.statusOrder.proof;
+
   const products = item.products;
 
-  console.log(item.products);
+  console.log(ocurrence, descripition);
 
   return (
-    <Container>
-      <Header title="Pedidos" />
-      <ContainerItem>
-        {status === "Viajando" ? (
-          <Truck width={52} height={52} fill={colorIcon} />
-        ) : status === "Entregue" ? (
-          <Success width={52} height={52} fill={colorIcon} />
-        ) : (
-          <Ocurrence width={52} height={52} fill={colorIcon} />
-        )}
-        <Column>
-          <Status>212121</Status>
-          <OrderNumber>23235235</OrderNumber>
-        </Column>
-      </ContainerItem>
-      <Spacing height={10} />
-      <ContainerPosition style={shadow.primary}>
-        <Location width={52} height={41} fill={colorIcon} />
-        <TextLocation>Última posição</TextLocation>
-      </ContainerPosition>
-      <Spacing height={10} />
-      <ContainerProducts style={shadow.primary}>
-        <TitleProducts>Produtos do pedido</TitleProducts>
-        <Row>
-          <Box width={52} height={30} fill={colorIcon} />
-        </Row>
-        <ColumnProducts>
+    <Scroll>
+      <Container>
+        <Header title="Pedidos" />
+        <Spacing height={10} />
+        <ContainerItem style={shadow.primary}>
+          {status === "Viajando" ? (
+            <Truck width={52} height={52} fill={colorIcon} />
+          ) : status === "Entregue" ? (
+            <Success width={52} height={52} fill={colorIcon} />
+          ) : (
+            <Ocurrence width={52} height={52} fill={colorIcon} />
+          )}
+          <Column>
+            <Status>212121</Status>
+            <OrderNumber>23235235</OrderNumber>
+          </Column>
+        </ContainerItem>
+        <Spacing height={10} />
+        <ContainerPosition style={shadow.primary}>
+          <Location width={52} height={41} fill={colorIcon} />
+          <TextLocation>Última posição</TextLocation>
+        </ContainerPosition>
+        <Spacing height={10} />
+        <ContainerProducts style={shadow.primary}>
+          <TitleProducts>Produtos do pedido</TitleProducts>
           <FlatList
             data={products}
             keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <Products>{item.product}</Products>}
+            renderItem={({ item }) => (
+              <Products>
+                Cód: {item.id} {item.product}
+                {"\n"}Quantidade:{item.quantity} {item.unit}
+              </Products>
+            )}
           />
-        </ColumnProducts>
-      </ContainerProducts>
-      <ContainerPod />
-      <ContainerOcurrence />
-    </Container>
+        </ContainerProducts>
+
+        {proof !== null ? (
+          <>
+            <Spacing height={10} />
+            <ContainerPod
+              style={shadow.primary}
+              onPress={() => {
+                Linking.openURL(proof);
+              }}
+            >
+              <Pod width={41} height={30} fill={colorIcon} />
+              <TitlePod>Comprovante já disponível</TitlePod>
+            </ContainerPod>
+          </>
+        ) : null}
+        <Spacing height={10} />
+        {ocurrence !== null ? (
+          <ContainerOcurrence style={shadow.primary}>
+            <Row>
+              <Ocurrence width={35} height={35} fill={colorIcon} />
+              <TitleOcurrence>Ocorrencia</TitleOcurrence>
+            </Row>
+            <DescriptionOcurrence>{ocurrence}</DescriptionOcurrence>
+            <DescriptionOcurrence>{descripition}</DescriptionOcurrence>
+          </ContainerOcurrence>
+        ) : null}
+        <Spacing height={10} />
+      </Container>
+    </Scroll>
   );
 }
--- src/screens/OrderDetails/styles.js
+++ src/screens/OrderDetails/styles.js
@@ -4,18 +4,21 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export const Container = styled(SafeAreaView)`
   flex: 1;
 `;
+
+export const Scroll = styled.ScrollView`
+`;
 export const ContainerItem = styled.View`
   padding-left: 15px;
   width: 95%;
   height: 75px;
   align-self: center;
-  background-color: pink;
   flex-direction: row;
   align-items: center;
+  color: ${({ theme }) => theme.colors.secondary};
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 export const StatusImage = styled.Image``;
 export const Column = styled.View`
-  background-color: blanchedalmond;
   margin-left: 34px;
 `;
 export const Status = styled.Text`
@@ -28,7 +31,6 @@ export const OrderNumber = styled.Text`
   color: ${({ theme }) => theme.colors.secondary};
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
-
 export const ContainerPosition = styled.View`
   width: 95%;
   height: 50px;
@@ -48,44 +50,72 @@ export const TextLocation = styled.Text`
 export const ContainerProducts = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
   width: 95%;
-  height: 200px;
+  height: 250px;
   align-self: center;
   border-radius: 3px;
+  padding: 15px 0px;
 `;
 export const FlatList = styled.FlatList`
-    flex: 1;
+  flex: 1;
 `;
 export const TitleProducts = styled.Text`
   font-size: 18px;
-  margin-bottom: 15px;
-  margin-left: 15px;
+  padding-left: 15px;
+  border-bottom-color: ${({ theme }) => theme.colors.secondary};
+  border-bottom-width: 0.5px;
   color: ${({ theme }) => theme.colors.secondary};
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
-export const Row = styled.View`
-  margin-left: 15px;
-  flex: 1;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: row;
-  background-color: azure;
-`;
 export const ColumnProducts = styled.View`
-    margin-left: 34px;
-    flex: 1;
+  margin-left: 34px;
+  flex: 1;
 `;
 export const Products = styled.Text`
   font-size: 18px;
   margin-top: 10px;
+  width: 95%;
+  padding-left: 20px;
+  align-items: center;
   color: ${({ theme }) => theme.colors.secondary};
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
-
-export const ContainerPod = styled.View`
-  min-height: 100px;
-  background-color: aquamarine;
+export const ContainerPod = styled.TouchableOpacity`
+  width: 95%;
+  height: 50px;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 3px;
+  align-self: center;
+  padding-left: 15px;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+export const TitlePod = styled.Text`
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.bold};
 `;
 export const ContainerOcurrence = styled.View`
-  min-height: 100px;
-  background-color: firebrick;
+  width: 95%;
+  border-radius: 3px;
+  align-self: center;
+  padding-left: 15px;
+  padding-bottom: 5px;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+export const TitleOcurrence = styled.Text`
+  font-size: 18px;
+  margin-left: 10px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+export const DescriptionOcurrence = styled.Text`
+  font-size: 18px;
+  margin-left: 10px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+export const Row = styled.View`
+  margin:10px;
+  flex-direction: row;
+  align-items: center;
 `;
